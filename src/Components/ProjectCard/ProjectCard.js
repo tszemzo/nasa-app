e853f05c44818a4c650ac604c4163875f2bb7109
Tomas Szemzo <szemzotomas@gmail.com>
2020-02-07 03:45:03 -0300

    Configurable number of project cards done

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,51 +1,67 @@
 import React from 'react';
+import config from '../../config/config';
+
+const server_url = config.SERVER_URL;
 
 class ProjectCard extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            project: {}
+        }
     }
 
     componentDidMount(){
-        console.log(this.props)
+        this.getProject(this.props.id)
     }
 
-  // startDate() {
-  //   return new Date(this.props.event.startDate);
-  // }
+    getProject(id) {
+        const route = '/api/projects/'
+        const auth = '?api_key=' + config.API_KEY
 
-  // endDate() {
-  //   return new Date(this.props.event.endDate);
-  // }
-
-  parseLastUpdated() {
-    const options = { year: 'numeric', month: 'short' };
-    return new Date(this.props.project.lastUpdated).toLocaleDateString(undefined, options)
-  }
-
-  parseDescriptionLength(){
-    const MAX_CHARS = 100
-    let description = this.props.project.description
-    if(description.length > MAX_CHARS){
-      description = description.substring(0, MAX_CHARS);
-      description += '...';
+        fetch(server_url + route + id + auth, {
+            method: 'get',
+        })
+        .then(response => response.json())
+        .then(data => {
+            let project = data.project
+            this.setState({
+              project: project,
+            })
+            console.log(this.state.project)
+        })
+        .catch((err) => {console.log(err)});
     }
-    return description;
-  }
 
-  render() {
-    return(
-        <div style={Card}>
-            <div style={Container}>
-                <h3><b>{this.props.project.title}</b></h3>
-                <p>Status: {this.props.project.status}</p> 
-                <p>Start Date: {this.props.project.startDate}</p>
-                <p>Last Update: {this.parseLastUpdated()}</p> 
-                <p style={Description}>Description: {this.parseDescriptionLength()}</p>
-                <button>More</button>
+    parseLastUpdated() {
+        const options = { year: 'numeric', month: 'short' };
+        return new Date(this.state.project.lastUpdated).toLocaleDateString(undefined, options)
+    }
+
+    parseDescriptionLength(){
+        const MAX_CHARS = 100
+        let description = this.state.project.description
+        if(description && description.length > MAX_CHARS){
+          description = description.substring(0, MAX_CHARS);
+          description += '...';
+        }
+        return description;
+    }
+
+    render() {
+        return(
+            <div style={Card}>
+                <div style={Container}>
+                    <h3><b>{this.state.project.title}</b></h3>
+                    <p>Status: {this.state.project.status}</p> 
+                    <p>Start Date: {this.state.project.startDate}</p>
+                    <p>Last Update: {this.parseLastUpdated()}</p> 
+                    <p style={Description}>Description: {this.parseDescriptionLength()}</p>
+                    <button>More</button>
+                </div>
             </div>
-        </div>
-    )
-  }
+        )
+    }
 }
 
 const Card = {
@@ -54,8 +70,10 @@ const Card = {
     backgroundColor: 'white',
     transition: '0.3s',
     borderRadius: '5px',
+    minHeight: '350px',
     maxHeight: '350px',
-    maxWidth: '400px'
+    maxWidth: '450px',
+    margin: '2rem',
 }
 
 const Container = {
--- src/Pages/NasaTechport/NasaTechport.js
+++ src/Pages/NasaTechport/NasaTechport.js
@@ -8,22 +8,39 @@ class NasaTechport extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			projects: []
+			projects: [],
 		}
 	}
 
 	componentDidMount() {
-		fetch(server_url + '/api/projects/95138' + '?api_key=' + config.API_KEY, {
+		this.getProjects()
+	}
+
+	getProjects() {
+		const route = '/api/projects'
+		const auth = '?api_key=' + config.API_KEY
+		const projectsAmount = 4
+
+		fetch(server_url + route + auth, {
 			method: 'get',
 		})
 		.then(response => response.json())
 		.then(data => {
+			let allProjects = data.projects.projects
+			let projectIds = this.getProjectIds(allProjects, projectsAmount)
 			this.setState({
-			  projects: [data.project],
+			  projects: projectIds,
 			})
-			console.log(this.state.projects);
 		})
 		.catch((err) => {console.log(err)});
+	}
+
+	getProjectIds(projects, amount) {
+		let projectIds = []
+		for (let i = 0; i < amount; i++) {
+			projectIds.push(projects[i].id)
+		}
+		return projectIds
 	}
 
 	render(){
@@ -34,7 +51,7 @@ class NasaTechport extends React.Component {
 				</div>
 				<main style={Main}>
 					<div style={Projects}>
-			            { this.state.projects.map((project, i) => <ProjectCard key={i} project={project} /> ) }
+			            { this.state.projects.map((projectId, i) => <ProjectCard id={projectId} key={i} /> ) }
 			        </div>
 				</main>
 			</div>
@@ -43,15 +60,16 @@ class NasaTechport extends React.Component {
 }
 
 const Projects = {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginBottom: '100px',
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'center',
     flexWrap: 'wrap',
     alignItems: 'flex-end',
     overflow: 'auto',
     width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: '100px',
 }
 
 const Main = {
